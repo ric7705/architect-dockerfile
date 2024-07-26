366af7ec41e27b2692592047b8759c38c421239e
macminiceiere <macminiceiere@gmail.com>
2024-07-26 23:00:25 +0800

    u

--- gitlab/run.sh
+++ gitlab/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 設置 GITLAB_HOME 環境變數
-export GITLAB_HOME=/srv/gitlab
+export GITLAB_HOME=/Users/ric-macmini7705/documents/code/architect/mounting/gitlab-home
 
 # 使用 docker-compose 啟動服務
 docker-compose up -d
